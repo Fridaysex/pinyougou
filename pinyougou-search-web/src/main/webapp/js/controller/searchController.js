097d364c8e3246b2985f6fc5df6b7a675f2a0c89
Fridaysex <wzpmylove@163.com>
2019-07-26 21:17:45 +0800

    第十三版:solr_recentprofect

--- pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,11 +1,9 @@
 app.controller('searchController',function ($scope,searchService) {
     //搜索
     $scope.search=function () {
-        alert("zhixing");
         searchService.search($scope.searchMap).success(
             function (response) {
-                $scope.resulMap=response;//搜索返回的结果
-                alert($scope.resulMap);
+                $scope.resultMap=response;//搜索返回的结果
                 buildPageLabel();
             }
         )
@@ -13,7 +11,7 @@ app.controller('searchController',function ($scope,searchService) {
 
 
     //搜索条件封装对象
-    $scope.searchMap={'keywords':'','category':'','brand':'','spec':'{}','price':'','pageNo':'1','pageSize':'40'};
+    $scope.searchMap={'keywords':'','category':'','brand':'','spec':{},'price':'','pageNo':1,'pageSize':40};
     //添加搜索项
     $scope.addSearchItem=function (key, value) {
         if (key=='category'||key=='brand'||key=='price'){//如果点击的是分类或者品牌/价格
@@ -34,24 +32,28 @@ app.controller('searchController',function ($scope,searchService) {
     };
 
     //构建分页标签()totalPages为总页数,始终以当前页为中心页,分情况改变起始页和结束页
+
     buildPageLabel=function () {
         $scope.pageLabel=[];//新增分页栏属性
-        var maxPageNo=$scope.resulMap.totalPages;//得到最大页码
+        var maxPageNo=$scope.resultMap.totalPages;//得到最大页码
         var firstPage=1;//开始页码
         var lastPage=maxPageNo;//截止页码
-        if($scope.resulMap.totalPages>5){//如果总页数大于5,显示部分页码
-            if ($scope.resulMap.pageNo<3){//如果当前页小于3
+        if($scope.resultMap.totalPages>5){//如果总页数大于5,显示部分页码
+            if ($scope.searchMap.pageNo<3){//如果当前页小于3
                 lastPage=5;//前5页
-            }else if($scope.resulMap.pageNo>=lastPage-2){//如果当前页大于等于最大页码-2
+            }else if($scope.searchMap.pageNo>=lastPage-2){//如果当前页大
+
+                // 于等于最大页码-2
                 firstPage=maxPageNo-4;
             }else {
-                firstPage=$scope.resulMap.pageNo-2;
-                lastPage=$scope.resulMap.pageNo+2;
+                firstPage=$scope.searchMap.pageNo-2;
+                lastPage=$scope.searchMap.pageNo+2;
             }
         }
         //循环产生页码标签
         for (var i=firstPage;i<=lastPage;i++){
             $scope.pageLabel.push(i);
+            alert(i)
         }
     }
 });
--- pinyougou-search-web/src/main/webapp/js/service/searchService.js
+++ pinyougou-search-web/src/main/webapp/js/service/searchService.js
@@ -1,6 +1,5 @@
 app.service('searchService',function ($http) {
     this.search=function (searchMap) {
-        alert("fuwu")
         return $http.post('itemsearch/search.do',searchMap);
     }
 });
